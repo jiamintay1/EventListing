0671e293c76566b488fa3f7108a13f783ec2ad87
Jia Min <jiamintay1@gmail.com>
2016-08-29 13:28:15 +0000

    Sign in to edit/destroy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  
+
   def distance(date)
     diff = date - Date.today
-    if diff==0 
+    if diff==0
       'Today is the day!'
     elsif diff==1
       'Event is in 1 day!'
@@ -10,5 +10,5 @@ module ApplicationHelper
       "Event is in #{diff.to_i} days!"
     end
   end
-  
+
 end
